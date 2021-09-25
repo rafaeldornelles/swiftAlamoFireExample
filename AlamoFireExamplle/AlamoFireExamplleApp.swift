f6a801948eb0db909676af02a4a2af5db710ad40
rafael.dornelles <rafafd@hotmail.com>
2021-09-25 09:52:08 -0300

    Initial Commit

---
+++ AlamoFireExamplle/AlamoFireExamplleApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlamoFireExamplleApp.swift
+//  AlamoFireExamplle
+//
+//  Created by IOS SENAC on 9/25/21.
+//
+
+import SwiftUI
+
+@main
+struct AlamoFireExamplleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
